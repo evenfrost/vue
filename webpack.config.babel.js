2186429349864ac50bd102d317743851d5611cef
evenfrost <kisolov@gmail.com>
2017-01-29 17:46:40 +0300

    Bump packages, upgrade wepback to v2

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,11 +2,13 @@ import { resolve } from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+const { PORT } = process.env;
+
 const rootResolve = pathname => resolve(__dirname, pathname);
 
 export default {
   entry: [
-    'webpack-dev-server/client?http://localhost:5000',
+    'babel-polyfill',
     rootResolve('client/index.js'),
   ],
   output: {
@@ -16,26 +18,32 @@ export default {
   },
   devtool: 'inline-source-map',
   module: {
-    loaders: [{
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }, {
       test: /\.pug$/,
-      exclude: /node_modules/,
-      loader: 'pug-html',
-    }, {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-    }, {
-      test: /\.vue$/,
-      exclude: /node_modules/,
-      loader: 'vue',
+      loader: 'pug-html-loader',
     }, {
       test: /\.styl$/,
-      exclude: /node_modules/,
-      loader: 'style!css!stylus',
+      use: [
+        'style-loader',
+        'css-loader',
+        'stylus-loader',
+      ],
+    }, {
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: {
+        loaders: {
+          html: 'pug-html-loader',
+          css: 'style-loader!css-loader!stylus-loader',
+        },
+      },
     }],
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     new HtmlWebpackPlugin({
       template: `${rootResolve('client/index.pug')}`,
@@ -43,19 +51,17 @@ export default {
   ],
   resolve: {
     alias: {
-      vue$: 'vue/dist/vue.js',
+      vue$: 'vue/dist/vue.common.js',
     },
-    extensions: ['', '.js', '.vue'],
-    root: rootResolve('client'),
+    extensions: ['*', '.js', '.vue'],
+    modules: [
+      rootResolve('client'),
+      'node_modules',
+    ],
   },
   devServer: {
     contentBase: rootResolve('client'),
-    publicPath: '/',
-  },
-  vue: {
-    loaders: {
-      html: 'pug',
-      css: 'style!css!stylus',
-    },
+    compress: true,
+    port: +PORT || 3000,
   },
 };
